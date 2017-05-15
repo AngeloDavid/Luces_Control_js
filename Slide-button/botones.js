aedaace632ebdb074713da2d58e0f5513e9b3c70
AngeloDAvid <angelodavid95@hotmail.es>
2017-05-15 12:34:33 -0500

    slide button sin nombre

--- Slide-button/botones.js
+++ Slide-button/botones.js
@@ -1,9 +1,23 @@
-var ips=['192.168.100.101',
-    '192.168.100.102'];
+var ips=[['192.168.100.101','Cuarto 1'],
+    ['192.168.100.102','Cuarto 2'],
+    ['192.168.100.103','Cuarto 3'],
+    ['192.168.100.104','Cuarto 4']];
 var ipActual;
 function elegir_Boton(boton) {
 	console.log(boton.value);
 	ipActual=boton.value;
+	if(ipActual==-1){
+	    document.getElementById("Mensaje").innerText="Todos" ;
+        document.getElementById("btn_Elegir").classList.add("activo");
+        for (i=0;i<ips.length;i++){
+            console.log(document.getElementById("option"+i));
+        }
+
+    }else{
+        document.getElementById("Mensaje").innerText=ips[ipActual][1];
+        document.getElementById("btn_Elegir").classList.remove("activo");
+    }
+	document.getElementById("btn_opciones").removeAttribute("disabled");
 }
 function boton_Elegido(range,ip) {
 	var val = range.value;    
@@ -11,7 +25,7 @@ function boton_Elegido(range,ip) {
     if (val == 1) {
         range.className = "rangeFalse";
         if ("WebSocket" in window){
-            var ws1 = new WebSocket("ws://"+ips[ip]+"/mas");
+            var ws1 = new WebSocket("ws://"+ips[ip][0]+"/mas");
             ws1.onclose = function(){ };
         }        
         
@@ -19,7 +33,7 @@ function boton_Elegido(range,ip) {
         //change color of slider background
         range.className = "rangeNeutral";
         if ("WebSocket" in window){
-            var ws1 = new WebSocket("ws://"+ips[ip]+"/off");
+            var ws1 = new WebSocket("ws://"+ips[ip][0]+"/off");
             ws1.onclose = function(){ };
         }
 
@@ -27,7 +41,7 @@ function boton_Elegido(range,ip) {
     	//change color of slider background
     	range.className = "rangeTrue";
         if ("WebSocket" in window){
-            var ws1 = new WebSocket("ws://"+ips[ip]+"/menos");
+            var ws1 = new WebSocket("ws://"+ips[ip][0]+"/menos");
             ws1.onclose = function(){ };
         }
     }
@@ -35,13 +49,14 @@ function boton_Elegido(range,ip) {
 
 function togglebutton(range) {
 	if(ipActual==-1){
-		Control_todos(range);
+        Control_todos(range);
 	}else{
-		boton_Elegido(ipActual);
+        boton_Elegido(range,ipActual);
 	}
 }
 
 function Control_todos(range) {
+    // console.log(range.value);
 	for (var i = 0; i <= ips.length; i++) {
 		boton_Elegido(range,i);
 	}
